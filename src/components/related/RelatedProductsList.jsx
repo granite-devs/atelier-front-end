782bc0810411b9a90c0eddb9fa12b8526fded5c3
Andrew Carnero <andrew.carnero@gmail.com>
2021-12-27 17:29:55 -0500

    create ProductCard component that displays the product card's id

--- src/components/related/RelatedProductsList.jsx
+++ src/components/related/RelatedProductsList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import API_KEY from '../../config.js';
+import ProductCard from './ProductCard.jsx';
 
 
 class RelatedProductsList extends React.Component {
@@ -14,40 +15,31 @@ class RelatedProductsList extends React.Component {
     this.getRelatedIds = this.getRelatedIds.bind(this);
   }
 
-  componentDidUpdate() {
-    console.log('relatedIds state: ', this.state.relatedIds);
-
-      this.getRelatedIds(this.props.productId);
+  componentDidMount() {
+    this.getRelatedIds(this.props.productId);
   }
 
   getRelatedIds(productIdToGet) {
-    var relatedIdsRequestConfig = {
-      method: 'get',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/products/${productIdToGet}/related`,
-      headers: {
-        'Authorization': API_KEY
-      }
-    };
+      var relatedIdsRequestConfig = {
+        method: 'get',
+        url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/products/${productIdToGet}/related`,
+        headers: {Authorization: API_KEY}
+      };
 
-    axios(relatedIdsRequestConfig)
-    .then((response) => {
-      console.log('successful GET')
-      if (this.state.relatedIds.toString() !== response.data.toString()) {
-        console.log('setting state');
+      axios(relatedIdsRequestConfig)
+      .then((response) => {
         this.setState({relatedIds: response.data});
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
     return (
       <div className=''>
         {this.state.relatedIds.map((relatedId, i) => {
-          //return <ProductCard key={i} />
-          return relatedId + ' ';
+          return <ProductCard key={i} productCardId={relatedId} />
         })}
       </div>
     );
