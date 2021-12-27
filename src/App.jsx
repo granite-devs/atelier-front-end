d32d8096aa8a36f4ae3a8cdb7c6077ca6daedb80
Heine574 <johannes@niemela.se>
2021-12-27 09:12:09 -0600

    Chore: fixed quotation problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 
-import React from "react";
-import Overview from "./components/overview/Overview.jsx";
-import Reviews from "./components/reviews/Reviews.jsx";
-import Questions from "./components/questions/Questions.jsx";
-import Related from "./components/related/Related.jsx";
+import React from 'react';
+import Overview from './components/overview/Overview.jsx';
+import Reviews from './components/reviews/Reviews.jsx';
+import Questions from './components/questions/Questions.jsx';
+import Related from './components/related/Related.jsx';
 
 class App extends React.Component {
   constructor(props) {
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.jsx";
-import "./styles.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './styles.scss';
 
 
-var mountNode = document.getElementById("app");
+var mountNode = document.getElementById('app');
 ReactDOM.render(<App name="Jane" />, mountNode);
