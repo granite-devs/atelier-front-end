ae698732ecef1661574fb7131ed3f35a5837d62d
Heine574 <johannes@niemela.se>
2021-12-27 17:36:19 -0600

    Implementation: ReviewsList component

---
+++ src/components/reviews/ReviewTile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+import API_KEY from '../../config.js';
+
+class ReviewTile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    const { review } = this.props;
+    return (
+      <div className='reviewTile'>
+        <h3>{review.summary}</h3>
+      </div>
+    );
+  }
+}
+
+export default ReviewTile;
--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import API_KEY from '../../config.js';
+import ReviewsList from './ReviewsList.jsx';
 
 class Reviews extends React.Component {
   constructor(props) {
@@ -8,38 +9,57 @@ class Reviews extends React.Component {
     this.state = {
       reviewsList: [],
       filteredReviewsList: [],
-      filter: () => true // Default "filter": displays all reviews.
+      filter: () => true, // Default "filter": displays all reviews.
+      reviewsPerLoad: 2,
+      reviewCount: 0,
+      moreToLoad: true,
+      isLoading: false
     };
-    this.loadReviews = this.loadReviews.bind(this);
+    this.loadMoreReviews = this.loadMoreReviews.bind(this);
     this.setFilter = this.setFilter.bind(this);
   }
 
   componentDidMount() {
-    this.loadReviews(1, 2);
+    this.fetchReviews(1, this.state.reviewsPerLoad);
   }
 
-  loadReviews(page, count) {
-    const intializationConfig = {
-      method: 'get',
-      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/products',
-      headers: {
-        Authorization: API_KEY,
-      },
-      params: {
-        page,
-        count,
-        'product_id': this.props.productId,
-        'sort': 'newest'
-      }
-    };
+  fetchReviews(page, count, reviewsList = []) {
+    const { productId } = this.props;
+    const { isLoading } = this.state;
+    if (productId && !isLoading) {
+      const intializationConfig = {
+        method: 'get',
+        url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/reviews',
+        headers: {
+          Authorization: API_KEY,
+        },
+        params: {
+          page,
+          count,
+          'sort': 'newest',
+          'product_id': this.props.productId
+        }
+      };
 
-    axios(intializationConfig)
-      .then((response) => {
-        this.setAndFilterReviews(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      console.log(intializationConfig);
+
+      this.setState({ isLoading: true });
+      axios(intializationConfig)
+        .then((response) => {
+          console.log(response.data);
+          this.setAndFilterReviews(reviewsList, response.data.results);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+
+  loadMoreReviews() {
+    const { reviewCount, reviewsList, reviewsPerLoad } = this.state;
+    const page = (reviewCount + reviewsPerLoad) / reviewsPerLoad;
+    console.log(page);
+    this.fetchReviews(page, reviewsPerLoad, reviewsList);
   }
 
   // setFilter ( function `filter` )
@@ -50,7 +70,20 @@ class Reviews extends React.Component {
     });
   }
 
-  setAndFilterReviews(reviewsList) {
+  setAndFilterReviews(oldReviews, newReviews) {
+    const reviewIds = {}; // keep track of IDs already in the list to eliminte duplicates
+    const reviewsList = [];
+    for (let review of oldReviews) {
+      reviewsList.push(review);
+      reviewIds[review.review_id] = true;
+    }
+    for (let review of newReviews) {
+      if (!reviewIds[review.review_id]) {
+        reviewsList.push(review);
+      }
+    }
+    const reviewCount = reviewsList.length;
+    const moreToLoad = newReviews.length === this.state.reviewsPerLoad;
     const filter = this.state.filter;
     const filteredReviewsList = [];
     for (let review of reviewsList) {
@@ -58,16 +91,21 @@ class Reviews extends React.Component {
         filteredReviewsList.push(review);
       }
     }
-    this.setState({ reviewsList, filteredReviewsList });
+    this.setState({ reviewsList, filteredReviewsList, reviewCount, moreToLoad, isLoading: false });
   }
 
   render() {
     const { name } = this.props;
+    const { moreToLoad } = this.state;
     return (
       <>
         <h1>
-          Hello {name}
+          Ratings &amp; Reviews {name}
         </h1>
+        <ReviewsList
+          loadMoreReviews={moreToLoad ? this.loadMoreReviews : null}
+          reviews={this.state.filteredReviewsList}
+        />
       </>
     );
   }
