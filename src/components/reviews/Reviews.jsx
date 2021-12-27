e67361b960292b77d03510151503ac89253e77dc
Heine574 <johannes@niemela.se>
2021-12-27 10:36:05 -0600

    Implementation: added data storage and basic filter function to reviews module

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 
 class Reviews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviewsList: [],
+      filteredReviewsList: []
+    };
+    this.setFilter = this.setFilter.bind(this);
+  }
+
+  componentDidMount() {
+    // TODO - load review data on component load
+    this.setFilter( () => true ); // Default "filter": displays all reviews.
+  }
+
+  // setFilter ( function `filter` )
+  // filters the displayed reviews according to the inputted filter function
+  setFilter(filter) {
+    const reviewsList = this.state.reviewsList;
+    const filteredReviewsList = [];
+    for (let review of reviewsList) {
+      if (filter(review)) {
+        filteredReviewsList.push(review);
+      }
+    }
+    this.setState({ filteredReviewsList });
+  }
+
   render() {
     const { name } = this.props;
     return (
