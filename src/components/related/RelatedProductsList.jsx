a320dcaf192769d20c946c6cc59104113e48c865
Andrew Carnero <andrew.carnero@gmail.com>
2021-12-27 14:02:20 -0500

    implementation: built RelatedProductsList component framework

---
+++ src/components/related/RelatedProductsList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Related extends React.Component {
+  render() {
+    const { name } = this.props;
+    return (
+      <>
+        <h1>
+          Hello {name}
+        </h1>
+      </>
+    );
+  }
+}
+
+export default Related;
