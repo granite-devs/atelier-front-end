36d99dae8176433497f7b401c0cc4e59190a69aa
Andrew Carnero <andrew.carnero@gmail.com>
2021-12-27 17:06:44 -0500

    slight tweaks

--- src/components/related/Related.jsx
+++ src/components/related/Related.jsx
@@ -1,7 +1,6 @@
 import RelatedProductsList from './RelatedProductsList.jsx';
 
 const Related = ({ productId, updateAppProductId }) => {
-
   return (
     <div>
       <RelatedProductsList
--- src/components/related/RelatedProductsList.jsx
+++ src/components/related/RelatedProductsList.jsx
@@ -14,24 +14,30 @@ class RelatedProductsList extends React.Component {
     this.getRelatedIds = this.getRelatedIds.bind(this);
   }
 
-  componentDidMount() {
-    this.getRelatedIds(this.props.productId)
+  componentDidUpdate() {
+    console.log('relatedIds state: ', this.state.relatedIds);
+
+      this.getRelatedIds(this.props.productId);
   }
 
-  getRelatedIds(productId) {
+  getRelatedIds(productIdToGet) {
     var relatedIdsRequestConfig = {
       method: 'get',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/products/${productId}/related`,
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/products/${productIdToGet}/related`,
       headers: {
         'Authorization': API_KEY
       }
     };
 
     axios(relatedIdsRequestConfig)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
+    .then((response) => {
+      console.log('successful GET')
+      if (this.state.relatedIds.toString() !== response.data.toString()) {
+        console.log('setting state');
+        this.setState({relatedIds: response.data});
+      }
     })
-    .catch(function (error) {
+    .catch((error) => {
       console.log(error);
     });
   }
@@ -39,8 +45,9 @@ class RelatedProductsList extends React.Component {
   render() {
     return (
       <div className=''>
-        {this.state.relatedIds.map(relatedId => {
-          return <ProductCard />
+        {this.state.relatedIds.map((relatedId, i) => {
+          //return <ProductCard key={i} />
+          return relatedId + ' ';
         })}
       </div>
     );
