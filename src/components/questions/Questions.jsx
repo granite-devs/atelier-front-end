06ccfb49d3a85aecd41dcd79d4ca2925d7bff31c
bryan <brynrmrzz@outlook.com>
2021-12-27 18:59:35 -0500

    feature: question parent component added

--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
 class Questions extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      productId: null
+    }
+  }
+
+  componentDidMount() {
+    const { productId } = this.props;
+    this.setState({productId: productId})
+  }
+
   render() {
-    const { name } = this.props;
     return (
       <>
-        <h1>
-          This man {name}
-        </h1>
+        <h3>
+          Questions & Answers
+        </h3>
+        {/* TO DO: Add Search Bar */}
+        {/* TO DO: Add QuestionsList */}
+        {/* TO DO: Load More Questions/Add Querstios */}
       </>
     );
   }
