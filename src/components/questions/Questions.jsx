3d4ecf068ab6c4c48c22be8d1d5b0625ca580ffe
bryan <brynrmrzz@outlook.com>
2021-12-27 14:05:34 -0500

    chore: refactored axios to es6 to remove type error

--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 class Questions extends React.Component {
   render() {
-    const { name } = this.props;
+    const { productId } = this.props;
     return (
       <>
         <h1>
-          This man {name}
+          This man {productId}
         </h1>
       </>
     );
