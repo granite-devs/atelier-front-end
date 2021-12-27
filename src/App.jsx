e268d7cff61915aa0a28cc67fc7a3cadb0eb5cb8
bryan <brynrmrzz@outlook.com>
2021-12-27 11:27:21 -0500

    implementation: added initial axios request

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
-
-import React from 'react';
-import Overview from './components/overview/Overview.jsx';
-import Reviews from './components/reviews/Reviews.jsx';
-import Questions from './components/questions/Questions.jsx';
-import Related from './components/related/Related.jsx';
+import React from "react";
+import axios from "axios";
+import Overview from "./components/overview/Overview.jsx";
+import Reviews from "./components/reviews/Reviews.jsx";
+import Questions from "./components/questions/Questions.jsx";
+import Related from "./components/related/Related.jsx";
+import API_KEY from "./config.js";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productId: 11 // TODO - find out what this should actually be.
+      productId: 11, // TODO - find out what this should actually be.
     };
     this.updatedProductId = this.updatedProductId.bind(this);
   }
 
   updatedProductId(productId) {
     this.setState({ productId });
+  }
+
+  componentDidMount() {
+
+    const config = {
+      method: "get",
+      url: "https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/products/39333",
+      headers: {
+        Authorization: API_KEY,
+      },
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        this.setState({ productId: response.data.id });
+      })
+
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   render() {
@@ -25,7 +47,10 @@ class App extends React.Component {
         <Overview productId={productId} />
         <Reviews productId={productId} />
         <Questions productId={productId} />
-        <Related productId={productId} updatedProductId={this.updatedProductId} />
+        <Related
+          productId={productId}
+          updatedProductId={this.updatedProductId}
+        />
       </div>
     );
   }
---
+++ src/config.ex.js
@@ -0,0 +1,3 @@
+const API_KEY = 'ur_key'
+
+export default API_KEY;
