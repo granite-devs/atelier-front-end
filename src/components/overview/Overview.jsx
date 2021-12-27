ee592695ebad1a530bfb4840b9f079ab966b6a8b
ByungChan Lee <lbc2415@gmail.com>
2021-12-27 16:21:09 -0600

    Overview: switched to function using react hook and implemented basic axios request function

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import App from '../../App.jsx';
+import axios from 'axios';
+import API_KEY from '../../config.js';
 
-class Overview extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-      </>
-    );
-  }
-}
+const Overview = ({productId}) => {
+  const [state, updateState] = useState({
+    productById: productId
+  });
+
+  const apiInstance = axios.create({
+    baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/products/',
+    headers: { Authorization: API_KEY },
+  });
+
+  useEffect (() => {
+    console.log(productId); // -> null
+    console.log(state.productById); // -> null
+    const getAllProductData = () => apiInstance.get(`${state.productById}/styles`);
+    getAllProductData()
+      .then((result) => {
+        console.log(result.data);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>
+        Hello {productId}
+      </h1>
+    </>
+  );
+
+};
+
 
 export default Overview;
