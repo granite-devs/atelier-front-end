abd25af8ea4583bf97f35b9b1a6ef9864d578cf7
ByungChan Lee <lbc2415@gmail.com>
2021-12-28 17:24:51 -0600

    implementation: fixed some css style

--- src/components/overview/ImageGallery/DefaultView.jsx
+++ src/components/overview/ImageGallery/DefaultView.jsx
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 const DefaultView = ({photos}) => {
   const [ isShowing, setIsShowing] = useState([]);
-  const [ mainImage, setMainImage ] = useState([]);
+  const [ mainImage, setMainImage ] = useState(false);
 
   useEffect (() => {
     if (photos) {
       setIsShowing(photos);
     }
   });
-
 
   const updateMainImage = (event) => {
     event.preventDefault();
@@ -18,23 +17,40 @@ const DefaultView = ({photos}) => {
   };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column'
-      }}>
-      {isShowing.map((element, idx) => {
-        return <img
-          id = {idx}
-          style = {{
-            width: '9vh',
-            height: '9vh',
-            cursor: 'pointer',
-            objectFit: 'cover'
-          }}
-          onClick = {updateMainImage}
-          src={element.url}/>;
-      })}
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column'
+        }}>
+        {isShowing.map((element, idx) => { //render all style photos
+          return <img
+            id = {idx}
+            style = {{
+              width: '9vh',
+              height: '9vh',
+              cursor: 'pointer',
+              justifyContent: 'space-around'
+            }}
+            onClick = {updateMainImage}
+            src={element.url}/>;
+        })}
+      </div>
+      <div //render the main style photo (default: the first style photo)
+        style={{
+          display: 'flex',
+          backgroundPosition: 'center',
+          backgroundSize: 'contain',
+          width: '100vh',
+          height: '90vh',
+          cursor: 'zoom-in',
+          objectFit: 'cover'
+        }}>
+        {(isShowing[0] !== undefined && mainImage === false) ?
+          <img id = 'main' src = {isShowing[0].url}/> :
+          <img id = 'main' src = {mainImage}/>
+        }
+      </div>
     </div>
   );
 };
