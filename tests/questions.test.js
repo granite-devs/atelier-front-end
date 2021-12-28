153fa20786e3edac3d75495f537df95481cd4663
brynrmrzz <brynrmrz@outlook.com>
2021-12-28 11:05:27 -0500

    chores: laid some tests to do to guide me

--- tests/questions.test.js
+++ tests/questions.test.js
@@ -3,10 +3,23 @@ import renderer from 'react-test-renderer';
 import QuestionsAnswers from '../src/components/questions/QuestionsAnswers.jsx';
 
 
-test('Renders questions and answers with an h3 header', () => {
+// Parent componentnt
 
+test('Renders questions and answers with an h3 header', () => {
   const questionsAnswersComp = renderer.create(<QuestionsAnswers />);
   const questionTree = questionsAnswersComp.toJSON()
   expect(questionTree).toMatchSnapshot()
-
 })
+
+test.todo('Should have a search bar');
+test.todo('Should have questions list component');
+test.todo('Should have a link to load more questions')
+test.todo('should have an add questions button')
+
+//search bar
+test.todo('Should have an input with placeholder text');
+test.todo('Should filter questions list')
+test.todo('Should filter results after three characters')
+
+//questionsListComp
+test.todo('should have an add answer button')
