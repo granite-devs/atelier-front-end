c3c7675683988bc1027858c470cd863d9fec35e1
Andrew Carnero <andrew.carnero@gmail.com>
2021-12-29 13:48:33 -0500

    chore: prep test file for tests

--- tests/related.test.js
+++ tests/related.test.js
@@ -4,7 +4,29 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 configure({ adapter: new Adapter() });
 
 import RelatedProductsList from '../src/components/Related/RelatedProductsList';
+import ProductCard from '../src/components/Related/RelatedProductsList';
 
+describe("Product Cards", () => {
+  let wrapper, wrapperState, wrapperInstance;
+
+  beforeEach(() => {
+    wrapper = shallow(<ProductCard />);
+    wrapperState = wrapper.state();
+  });
+
+  it('is a stateful component', () => {
+    expect(wrapperState).not.toBeNull();
+  });
+
+  it('should render a ProductCard component for every item in the relatedIds list', () => {
+    const dummyIds = [1, 2, 3];
+
+    wrapper.setState({ relatedIds: dummyIds }, () => {
+      expect(wrapper.find('ProductCard')).toHaveLength(dummyIds.length);
+    });
+  });
+
+});
 
 describe("Related Products List", () => {
   let wrapper, wrapperState, wrapperInstance;
