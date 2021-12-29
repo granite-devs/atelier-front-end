467b3d1c37a87151c9d640a67d3fc3889c0ed6cb
Karl A. Johannes Niemelae <johannes@niemela.se>
2021-12-29 13:55:20 -0600

    Update shared.test.js

--- tests/shared.test.js
+++ tests/shared.test.js
@@ -19,35 +19,35 @@ describe('StarRating component', () => {
 });
 
 describe('Star component', () => {
-  test('a Star with with a rating fraction close to 1 should fill 100% of the star', () => {
+  test('a Star with a rating fraction close to 1 should fill 100% of the star', () => {
     const component = shallow( // mounts a `shallow` render of the component (does not render children)
       <Star fraction={1} />
     );
     expect(component.containsMatchingElement(<stop offset='100%'></stop>)).toBe(true);
   });
 
-  test('a Star with with a rating fraction close to 0.75 should fill 75% of the star', () => {
+  test('a Star with a rating fraction close to 0.75 should fill 75% of the star', () => {
     const component = shallow( // mounts a `shallow` render of the component (does not render children)
       <Star fraction={0.75} />
     );
     expect(component.containsMatchingElement(<stop offset='75%'></stop>)).toBe(true);
   });
 
-  test('a Star with with a rating fraction close to 0.5 should fill 50% of the star', () => {
+  test('a Star with a rating fraction close to 0.5 should fill 50% of the star', () => {
     const component = shallow( // mounts a `shallow` render of the component (does not render children)
       <Star fraction={0.5} />
     );
     expect(component.containsMatchingElement(<stop offset='50%'></stop>)).toBe(true);
   });
 
-  test('a Star with with a rating fraction close to 0.25 should fill 25% of the star', () => {
+  test('a Star with a rating fraction close to 0.25 should fill 25% of the star', () => {
     const component = shallow( // mounts a `shallow` render of the component (does not render children)
       <Star fraction={0.25} />
     );
     expect(component.containsMatchingElement(<stop offset='25%'></stop>)).toBe(true);
   });
 
-  test('a Star with with a rating fraction close to 0 should fill 0% of the star', () => {
+  test('a Star with a rating fraction close to 0 should fill 0% of the star', () => {
     const component = shallow( // mounts a `shallow` render of the component (does not render children)
       <Star fraction={0} />
     );
