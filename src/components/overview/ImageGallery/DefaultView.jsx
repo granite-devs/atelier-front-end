eba3fc9e1355271f5665e7227b4ca903a7beddbb
ByungChan Lee <lbc2415@gmail.com>
2021-12-30 00:22:49 -0600

    css: added borderBottom at each style photo when it is selected as main photo

--- src/components/overview/ImageGallery/DefaultView.jsx
+++ src/components/overview/ImageGallery/DefaultView.jsx
@@ -1,35 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
-const DefaultView = ({state, updateState, handleToExpand, handleRightArrow, handleLeftArrow}) => {
-
-  const updateMainImage = (event) => {
-    event.preventDefault();
-    let clickedImg = event.target.src;
-
-    updateState((preValues) => {
-      return {...preValues, mainImage: clickedImg};
-    });
-  };
-
+const DefaultView = ({state, updateState, updateMainImage, handleToExpand, handleRightArrow, handleLeftArrow}) => {
 
   return (
     <>
-    {(state !== undefined) && (
-      <div className='defaultView'>
-        <div className='styleImages'>
-          {state.styleImages.map((element, idx) => { //render all style photos
-            return <img id={idx} key={idx} class='eachStyleImage' onClick={updateMainImage} src={element.url}/>; })}
-        </div>
-        <div className='mainImage'>
-          <img onClick={handleLeftArrow} className='btnFloating' id='left' src ="https://img.icons8.com/ios-filled/50/000000/long-arrow-left.png"></img>
+      {(state !== undefined) && (
+        <div className='defaultView'>
+          <div className='styleImages'>
+            {state.styleImages.map((element, idx) => { //render all style photos
+              return <img id={idx} key={idx} className='eachStyleImage' onClick={updateMainImage} src={element.url}/>; })}
+          </div>
+          <div className='mainImage'>
+            <img onClick={handleLeftArrow} className='btnFloating' id='left' src ="https://img.icons8.com/ios-filled/50/000000/long-arrow-left.png"></img>
             {(state.styleImages[0] !== undefined && state.mainImage === '') ?
-              <img id = 'main' data-scale = '2' onClick = {handleToExpand} src = {state.styleImages[0].url}/> :
-              <img id = 'main' data-scale = '2' onClick = {handleToExpand} src = {state.mainImage}/>
+              <img id = 'main' onClick = {handleToExpand} src = {state.styleImages[0].url}/> :
+              <img id = 'main' onClick = {handleToExpand} src = {state.mainImage}/>
             }
-          <img onClick={handleRightArrow} className='btnFloating' id='right' src ="https://img.icons8.com/ios-filled/50/000000/long-arrow-right.png"></img>
+            <img onClick={handleRightArrow} className='btnFloating' id='right' src ="https://img.icons8.com/ios-filled/50/000000/long-arrow-right.png"></img>
+          </div>
         </div>
-      </div>
-    )}
+      )}
     </>
   );
 };
--- src/components/overview/ImageGallery/ImageGallery.jsx
+++ src/components/overview/ImageGallery/ImageGallery.jsx
@@ -2,7 +2,19 @@ import React, { useState, useEffect } from 'react';
 import DefaultView from './DefaultView.jsx';
 
 const ImageGallery = ({state, updateState}) => {
-  const [ currentStylePhotos, updateStylePhotos] = useState([]);
+  const [ currentStylePhotos, updateStylePhotos ] = useState([]);
+  const [ clickedImage, updateClickedImage ] = useState(0);
+
+  const updateMainImage = (event) => {
+    event.preventDefault();
+    let clickedImg = event.target.src;
+    let currentImgIndex = currentStylePhotos.indexOf(clickedImg);
+    updateClickedImage(currentImgIndex);
+
+    updateState((preValues) => {
+      return {...preValues, mainImage: clickedImg};
+    });
+  };
 
   const handleToExpand = () => {
     let mainImageElement = document.getElementById('main');
@@ -15,7 +27,7 @@ const ImageGallery = ({state, updateState}) => {
         return {...preValues, isExpanded: true};
       });
     } else {
-      mainImageElement.style.width = '75vh';
+      mainImageElement.style.width = '50vh';
       mainImageElement.style.cursor = 'zoom-in';
 
       updateState((preValues) => {
@@ -26,6 +38,18 @@ const ImageGallery = ({state, updateState}) => {
 
   useEffect(() => {
     helperForArrow();
+
+    //differenciate selected main image//
+    let currentImageElement = document.getElementById(clickedImage);
+    if (currentImageElement !== null) {
+      for (let i = 0; i < state.styleImages.length; i++) {
+        document.getElementById(i).style.borderStyle = 'none';
+      }
+      currentImageElement.style.borderBottom = 'solid';
+      currentImageElement.style.borderColor = 'rgb(211, 215, 255)';
+      currentImageElement.style.borderWidth = '5px';
+    }
+
   }, [state])
 
   const helperForArrow = () => {
@@ -44,6 +68,7 @@ const ImageGallery = ({state, updateState}) => {
       updateState((preValues) => {
         return {...preValues, mainImage: state.selectedStyle.photos[currentImgIndex + 1].url};
       });
+      updateClickedImage(currentImgIndex + 1);
     }
   }
 
@@ -55,6 +80,7 @@ const ImageGallery = ({state, updateState}) => {
       updateState((preValues) => {
         return {...preValues, mainImage: state.selectedStyle.photos[currentImgIndex - 1].url};
       });
+      updateClickedImage(currentImgIndex - 1);
     }
 
   }
@@ -68,6 +94,7 @@ const ImageGallery = ({state, updateState}) => {
         handleRightArrow={handleRightArrow}
         handleLeftArrow={handleLeftArrow}
         helperForArrow={helperForArrow}
+        updateMainImage={updateMainImage}
       />
     </>
   );
