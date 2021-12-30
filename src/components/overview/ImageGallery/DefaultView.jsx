99b34c0519424c9584a0031abf8b31dc2a4af5f9
ByungChan Lee <lbc2415@gmail.com>
2021-12-29 22:39:07 -0600

    moved every css style into style sheet from component file

--- src/components/overview/ImageGallery/DefaultView.jsx
+++ src/components/overview/ImageGallery/DefaultView.jsx
@@ -18,25 +18,14 @@ const DefaultView = ({state, updateState, handleToExpand, handleRightArrow, hand
       <div className='defaultView'>
         <div className='styleImages'>
           {state.styleImages.map((element, idx) => { //render all style photos
-            return <img
-              style = {{
-                width: '9vh',
-                height: '9vh',
-                cursor: 'pointer'
-              }}
-              id = {idx}
-              key = {idx}
-              class = 'eachStyleImage'
-              onClick = {updateMainImage}
-              src={element.url}/>;
-          })}
+            return <img id={idx} key={idx} class='eachStyleImage' onClick={updateMainImage} src={element.url}/>; })}
         </div>
         <div className='mainImage'>
           <img onClick={handleLeftArrow} className='btnFloating' id='left' src ="https://img.icons8.com/ios-filled/50/000000/long-arrow-left.png"></img>
-          {(state.styleImages[0] !== undefined && state.mainImage === '') ?
-            <img id = 'main' onClick = {handleToExpand} src = {state.styleImages[0].url} style = {{width: '75vh', height: '54vh'}}/> :
-            <img id = 'main' onClick = {handleToExpand} src = {state.mainImage} style = {{width: '75vh', height: '54vh'}}/>
-          }
+            {(state.styleImages[0] !== undefined && state.mainImage === '') ?
+              <img id = 'main' data-scale = '2' onClick = {handleToExpand} src = {state.styleImages[0].url}/> :
+              <img id = 'main' data-scale = '2' onClick = {handleToExpand} src = {state.mainImage}/>
+            }
           <img onClick={handleRightArrow} className='btnFloating' id='right' src ="https://img.icons8.com/ios-filled/50/000000/long-arrow-right.png"></img>
         </div>
       </div>
