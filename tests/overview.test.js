653eb96d543ac977598aff7f5ff3d3ee6a996a01
ByungChan Lee <lbc2415@gmail.com>
2021-12-30 23:55:50 -0600

    test: added test for product information

--- tests/overview.test.js
+++ tests/overview.test.js
@@ -8,17 +8,22 @@ import React from 'react';
 import DefaultView from '../src/components/overview/ImageGallery/DefaultView.jsx';
 import ImageGallery from '../src/components/overview/ImageGallery/ImageGallery.jsx';
 import Overview from '../src/components/overview/Overview.jsx';
+import ProductInformation from '../src/components/overview/ProductInformation/ProductInformation.jsx';
+
+let exampleData = {
+  selectedStyleDefaultImages: [
+    {url: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+    thumbnail_url: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80'},
+    {url: 'https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQ',
+    thumbnail_url: 'https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQ'}
+  ],
+  mainImage: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+  selectedProductName: 'Camo Onesie',
+  selectedProductCategory: 'Jackets',
+  selectedProductDefaultPrice: '140.00'
+}
 
 describe('DefaultView component', () => {
-  let exampleData = {
-    styleImages: [
-      {url: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
-      thumbnail_url: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80'},
-      {url: 'https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQ',
-      thumbnail_url: 'https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQ'}
-    ],
-    mainImage: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80'
-  }
 
   it('should render a page successfully', () => {
     const wrapper = shallow(<DefaultView/>);
@@ -39,11 +44,27 @@ describe('DefaultView component', () => {
 
 describe('Overview Product information component', () => {
 
+  it('should render a page successfully', () => {
+    const wrapper = shallow(<ProductInformation state={exampleData}/>);
+    expect(wrapper.length).toBe(1);
+    console.log(wrapper.debug());
+  });
 
-  it ('', () => {
+  it('should display a productName', () => {
+    const wrapper = shallow(<ProductInformation state={exampleData}/>);
+    expect(wrapper.find('#productName').text()).toEqual(exampleData.selectedProductName);
+  });
 
+  it('should display a category', () => {
+    const wrapper = shallow(<ProductInformation state={exampleData}/>);
+    expect(wrapper.find('#category').text()).toEqual(`[Category - ${exampleData.selectedProductCategory}]`);
+    console.log(wrapper.debug());
+  });
+
+  it('should display a price', () => {
+    const wrapper = shallow(<ProductInformation state={exampleData}/>);
+    expect(wrapper.find('#price').text()).toEqual(exampleData.selectedProductDefaultPrice);
+    console.log(wrapper.debug());
   });
 });
-
-
 
