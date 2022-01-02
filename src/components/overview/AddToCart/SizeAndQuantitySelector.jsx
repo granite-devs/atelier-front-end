11287b25d5021bf15b65e129daca1019acb7ff00
ByungChan Lee <lbc2415@gmail.com>
2022-01-01 22:26:53 -0600

    implementation : added size and quantity option table

--- src/components/overview/AddToCart/SizeAndQuantitySelector.jsx
+++ src/components/overview/AddToCart/SizeAndQuantitySelector.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
 const SizeAndQuantitySelector = ({state, updateState}) => {
-  const [ quantity, updateQuantity ] = useState([]);
   const [ size, updateSize ] = useState([]);
+  const [ quantity, updateQuantity ] = useState([]);
 
   useEffect (() => {
+    // added into state as an array to use map function for rendering.
     if (state.currentStyle.skus !== undefined) {
       const currentStyleSkus = state.currentStyle.skus;
       for (const key in currentStyleSkus) {
         quantity.push(state.currentStyle.skus[key].quantity);
+        quantity.sort((a, b) => { //sorting from samll quantity to large quantity
+          return a - b;
+        });
         updateQuantity(quantity);
 
         size.push(state.currentStyle.skus[key].size);
@@ -21,7 +25,18 @@ const SizeAndQuantitySelector = ({state, updateState}) => {
     <>
       {state.currentStyle.skus !== undefined && (
         <div id='sizeAndQuantitySelector'>
-          hello
+          <select id='size'>
+            <option id='default' key='10'>Select Size</option>;
+            {size.map((element, idx) => {
+              return <option id={`size${element}`} key={idx}>{element}</option>;
+            })}
+          </select>
+          <select id='quantity'>
+            <option id='default' key='10'>Quantity</option>;
+            {quantity.map((element, idx) => {
+              return <option id={`quantity${element}`} key={idx}>{element}</option>;
+            })}
+          </select>
         </div>
       )}
     </>
