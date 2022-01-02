3b57cfc6d045c48981770cd4cca59deb9229f6c2
ByungChan Lee <lbc2415@gmail.com>
2022-01-02 00:05:38 -0600

    implementation: added feature notifying the user about the stock availability

--- src/components/overview/AddToCart/SizeAndQuantitySelector.jsx
+++ src/components/overview/AddToCart/SizeAndQuantitySelector.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
 const SizeAndQuantitySelector = ({state, updateState}) => {
-  const [ sizeAndQuantity, updateSizeAndQuantity ] = useState([]);
+  const [ sizeAndQuantity, updateSizeAndQuantity ] = useState({});
+  const [ currentSize, updateCurrentSize ] = useState(null);
   const [ quantity, updateQuantity ] = useState([]);
 
   useEffect (() => {
@@ -9,33 +10,58 @@ const SizeAndQuantitySelector = ({state, updateState}) => {
     // the sizeAndQuanty would be changed when the user select the other style.
     if (state.currentStyle.skus !== undefined) {
       let currentStyleSkus = state.currentStyle.skus;
-      let currentStyleSkusArr = [];
+      let sizeAndQuantityObject = {};
       for (let key in currentStyleSkus) {
-        let sizeAndQuantityObject = {};
         sizeAndQuantityObject[state.currentStyle.skus[key].size] = state.currentStyle.skus[key].quantity;
-
-        currentStyleSkusArr.push(sizeAndQuantityObject);
       }
-      updateSizeAndQuantity(currentStyleSkusArr);
+      updateSizeAndQuantity(sizeAndQuantityObject);
     }
   }, [state.currentStyle]);
+
+  useEffect (() => {
+    if (currentSize !== null) {
+      let quantityAvailable = sizeAndQuantity[currentSize];
+      let arrayForQuantity = [];
+      for (let i = 1; i <= quantityAvailable; i++) {
+        arrayForQuantity.push(i);
+      }
+
+      updateQuantity(arrayForQuantity);
+    }
+  }, [currentSize]);
+
+  const updateSize = (event) => {
+    let selectedSize = event.target.value;
+    updateCurrentSize(selectedSize);
+  };
 
   return (
     <>
       {state.currentStyle.skus !== undefined && (
         <div id='sizeAndQuantitySelector'>
-          {/* <select id='size'>
-            <option id='default' key='10'>Select Size</option>;
-            {size.map((element, idx) => {
+          <select onChange={updateSize} id='size'>
+            <option id='default' key='size'>Select Size</option>
+            {Object.keys(sizeAndQuantity).map((element, idx) => {
               return <option id={`size${element}`} key={idx}>{element}</option>;
             })}
           </select>
           <select id='quantity'>
-            <option id='default' key='10'>Quantity</option>;
-            {quantity.map((element, idx) => {
-              return <option id={`quantity${element}`} key={idx}>{element}</option>;
-            })}
-          </select> */}
+            {quantity[0] !== undefined ?
+              quantity.map((element, idx) => {
+                return <option id={`quantity${element}`} key={idx}>{element}</option>;
+              }) :
+              <option id='default' key='default'>-</option>
+            }
+          </select>
+          {quantity[0] !== undefined && (
+            quantity.length > 9 ?
+              <div id='enoughStock' style={{color: '#3aba2c'}}>
+              In Stock
+              </div> :
+              <div id='lessThanTen' style={{color: '#f75d5d'}}>
+              Only {quantity.length} available!
+              </div>
+          )}
         </div>
       )}
     </>
