9946816986b82fe39078ac01128ca0ecd646cd46
ByungChan Lee <lbc2415@gmail.com>
2022-01-02 15:38:48 -0600

    implementation: AddToCart button onClick func when the user did not select size

--- src/components/overview/AddToCart/AddToCart.jsx
+++ src/components/overview/AddToCart/AddToCart.jsx
@@ -3,13 +3,21 @@ import SizeAndQuantitySelector from './SizeAndQuantitySelector.jsx';
 
 const AddToCart = ({state, updateState}) => {
 
+  const addToCartModal = () => {
+    let sizeValue = document.getElementById('size').value;
+    let quantityValue = document.getElementById('quantity').value;
+
+    if (sizeValue === 'default' || quantityValue === 'default') {
+      alert('You need to select a size and quantity first');
+    }
+  };
 
   return (
     <>
       {state !== undefined && (
         <div id='addToCart'>
           <SizeAndQuantitySelector state={state} updateState={updateState}/>
-          <button id='addToCartButton'>Add To Cart</button>
+          <button onClick={addToCartModal} id='addToCartButton'>Add To Cart</button>
         </div>
       )}
     </>
