992afa363d5252db231e2997a51cdba086f3fa5e
ByungChan Lee <lbc2415@gmail.com>
2022-01-03 17:20:02 -0600

    fixed error that map is not a function (productFeature)

--- src/components/overview/ProductInformation/ProductDesc.jsx
+++ src/components/overview/ProductInformation/ProductDesc.jsx
@@ -2,26 +2,26 @@ import React, { useState, useEffect } from 'react';
 
 const ProductDesc = ({state, updateState}) => {
 
+  let productFeature;
+  if (state.selectedProductFeature) {
+    productFeature = state.selectedProductFeature.map((element, idx) => {
+      return <div key={idx} style={{marginBottom: '2px'}}>{`- ${element.feature}: ${element.value}`}</div>;
+    });
+  }
 
   return (
     <>
-      {state !== undefined && (
-        <>
-          <div id='productDesc'>
-            <div id='slogan'>
-              {state.selectedProductSlogan}
-            </div>
-            <div id='productDescription'>
-              {state.selectedProductDesc}
-            </div>
-          </div>
-          <div id='productFeature'>
-            {state.selectedProductDetail.features.map((element) => {
-              return <div>{`${element.feature}: ${element.value}`}</div>;
-            })}
-          </div>
-        </>
-      )}
+      <div id='productDesc'>
+        <div id='slogan'>
+          {state.selectedProductSlogan}
+        </div>
+        <div id='productDescription'>
+          {state.selectedProductDesc}
+        </div>
+      </div>
+      <div id='productFeature'>
+        {productFeature}
+      </div>
     </>
   );
 };
