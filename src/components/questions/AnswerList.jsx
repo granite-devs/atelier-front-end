00da2f5ffc96daa05b993e02b4ca4df9675fbeb3
bryan <brynrmrzz@outlook.com>
2022-01-02 19:33:50 -0500

    chores: fixed redudnant A: label

--- src/components/questions/AnswerList.jsx
+++ src/components/questions/AnswerList.jsx
@@ -1,37 +1,40 @@
 import React from "react";
-import Answer from "./Answer.jsx"
+import Answer from "./Answer.jsx";
 
 const AnswerList = ({ answers, loadMoreAnswers, voteHelpfulAnswer }) => {
-
-  const arrayOfVisibleAnswers = answers.filter(answer => answer.isVisible ? answer : null)
+  const arrayOfVisibleAnswers = answers.filter((answer) =>
+    answer.isVisible ? answer : null
+  );
 
   return (
     <div className="answer-item">
-      {
-        arrayOfVisibleAnswers.map((answer) => {
-          return (
-            <div className='answer-label' key={answer.answer_id}>
-              <strong>A:</strong>
+      <div className="answer-label">
+        {
+          (answers.length > 0) && (<strong>A:</strong>)
+        }
+        <div>
+          {arrayOfVisibleAnswers.map((answer) => {
+            return (
               <Answer
                 key={answer.answer_id}
                 answer={answer}
                 voteHelpfulAnswer={voteHelpfulAnswer}
               />
-            </div>
-          )
-        })
-      }
+            );
+          })}
+        </div>
+      </div>
       <br></br>
-      {
-        (answers.length > 2) && (
-          <a
-            id="see-more-answers-btn"
-            onClick={() => {
-              loadMoreAnswers()
-            }}
-          > LOAD MORE ANSWERS </a>
-        )
-      }
+      {answers.length > 2 && (
+        <a
+          id="see-more-answers-btn"
+          onClick={() => {
+            loadMoreAnswers();
+          }}
+        >
+          LOAD MORE ANSWERS
+        </a>
+      )}
     </div>
   );
 };
