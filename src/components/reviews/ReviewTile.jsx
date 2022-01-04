bb0f07310ec356566dad78e1ef025d9a4f77c693
Heine574 <johannes@niemela.se>
2022-01-04 17:51:41 -0600

    Updated ReviewTile component to allow users to mark reviews as helpful

--- src/components/reviews/ReviewTile.jsx
+++ src/components/reviews/ReviewTile.jsx
@@ -19,8 +19,8 @@ class ReviewTile extends React.Component {
   }
 
   render() {
-    const { review } = this.props;
-    const { displayFullBody } = this.state;
+    const { review, hasVotedHelpul, voteReviewHelpful } = this.props;
+    const { displayFullBody, justVotedHelpful } = this.state;
     const bodyToDisplay = displayFullBody ? review.body : review.body.substr(0, 250);
     const moreToDisplay = bodyToDisplay.length < review.body.length;
     const showMoreBtn = review.body.length > 250 ? (
@@ -29,6 +29,23 @@ class ReviewTile extends React.Component {
         onClick={this.toggleFullBody}
       >Show {displayFullBody ? 'Less' : 'More'}</a>
     ) : '';
+    const voteHelpfulBtns = !hasVotedHelpul ? (
+      <div className='helpful'>
+        <span>Was this review helpful? </span>
+        <a onClick={() => {
+          review.helpfulness++;
+          voteReviewHelpful(review.review_id);
+        }}>
+          Yes ({review.helpfulness})
+        </a><span> / </span><a>
+          No
+        </a>
+      </div>
+    ) : (
+      <div className='helpful'>
+        <span>{review.helpfulness} users found this review useful</span>
+      </div>
+    );
     return (
       <div className='reviewTile'>
         <span className='name-date'>{review.reviewer_name}, {timeAgo(review.date)}</span>
@@ -36,6 +53,7 @@ class ReviewTile extends React.Component {
         <h3>{review.summary}</h3>
         <p>{bodyToDisplay}{moreToDisplay ? '...' : ''}</p>
         {showMoreBtn}
+        {voteHelpfulBtns}
       </div>
     );
   }
