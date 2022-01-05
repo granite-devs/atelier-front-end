1c4562fd0c08d44b7b430afa3a2e89e3a3e0566d
Andrew Carnero <andrew.carnero@gmail.com>
2022-01-05 10:41:25 -0500

    added getRatingFraction helper method

--- src/components/related/ProductCard.jsx
+++ src/components/related/ProductCard.jsx
@@ -11,20 +11,11 @@ class ProductCard extends React.Component {
     super(props);
 
     this.state = {
-      category: '...',
-      name: '...',
-      price: '...',
-      salePrice: '...',
-      rating: null,
-      primaryImg: null,
       features: [],
       displayModal: false,
-      currentItemFeatures: {name: '', features: []},
-      initialRequestMade: false
+      componentMounted: true
     }
     this.actionBtnClick = this.actionBtnClick.bind(this);
-
-    this.fetchProductDetails = this.fetchProductDetails.bind(this); //todo: delete
   }
 
   componentDidMount() {
@@ -32,135 +23,7 @@ class ProductCard extends React.Component {
     const cachedProduct = checkCache(productCardId);
 
     console.log(`--> product card ${productCardId} mounted!`);
-
-    this.fetchProductDetails(productCardId, 'currentRelatedItem');
-    //this.fetchProductDetails(productId, 'currentItem');
-  }
-
-  fetchProductDetails(productIdToGet, stateToUpdate = 'currentRelatedItem') {
-    const { initialRequestMade } = this.state;
-    const { updateAppProductId, productId, relatedId, checkCache } = this.props;
-
-    if (productIdToGet) {
-      const cachedProduct = checkCache(productIdToGet);
-
-
-      console.log('FETCHING ALL FOR PRODUCT ', productIdToGet);
-
-      if (cachedProduct) {
-        console.log(' CACHED PRODUCT: ----- ', cachedProduct);
-        console.log('pulling from the cache and setting state!!');
-
-        const { name } = cachedProduct.details;
-        let features = cachedProduct.details;
-
-        if (stateToUpdate === 'relatedItem') {
-          features.forEach(feature => {
-            feature['belongsTo'] = 'relatedItem';
-          });
-
-          const stateObject = this.stateBuilder(cachedProduct);
-
-          this.setState(stateObject);
-        }
-
-        if (stateToUpdate === 'currentItem') {
-          features.forEach(feature => {
-            feature['belongsTo'] = 'currentItem';
-          });
-
-          this.setState({
-            currentItemFeatures: {name: name, features: features}
-          });
-        }
-
-      } else {
-        const productRequestRequestConfig = {
-          method: 'get',
-          url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/products/${productIdToGet}`,
-          headers: {Authorization: API_KEY}
-        };
-
-        const stylesRequestConfig = {
-          method: 'get',
-          url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/products/${productIdToGet}/styles`,
-          headers: {Authorization: API_KEY}
-        };
-
-        const reviewsRequestConfig = {
-          method: 'get',
-          url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/reviews/meta?product_id=${productIdToGet}`,
-          headers: {Authorization: API_KEY}
-        };
-
-        const productRequest = axios(productRequestRequestConfig);
-        const stylesRequest = axios(stylesRequestConfig);
-        const reviewsRequest = axios(reviewsRequestConfig);
-
-        axios.all([productRequest, stylesRequest, reviewsRequest])
-          .then(axios.spread((...responses) => {
-            const productResponse = responses[0];
-            const stylesResponse = responses[1];
-            const reviewsResponse = responses[2];
-
-            let productObjectToCache = {
-              details: productResponse.data,
-              styles: stylesResponse.data,
-              reviews: reviewsResponse.data
-            }
-
-            updateAppProductId(productId, productObjectToCache, productIdToGet);
-
-            const stateObject = this.stateBuilder(productObjectToCache);
-
-            this.setState(stateObject);
-
-          }))
-          .catch(errors => {
-            console.log('error fetching requests!', errors);
-          })
-      }
-    }
-  }
-
-  stateBuilder(productCacheObject) {
-
-    const { name, category } = productCacheObject.details;
-    const { price, salePrice, results } = productCacheObject.styles;
-    const { ratings } = productCacheObject.reviews;
-
-    let features = productCacheObject.details.features;
-
-      features.forEach(feature => {
-        feature['belongsTo'] = 'relatedItem';
-      });
-
-      const ratingFraction = () => {
-        let ratingFraction;
-        let [count, sum] = [0, 0];
-
-        for (let key in ratings) {
-          count += parseInt(ratings[key]);
-          sum += parseInt(key) * parseInt(ratings[key]);
-        }
-
-        const average = sum / count;
-        if (average > 0 ) { return average };
-
-        return 0;
-      };
-
-      const stateObject = {
-        name: name,
-        category: category,
-        features: features,
-        price: results[0].original_price,
-        salePrice: results[0].sale_price,
-        primaryImg: results[0].photos[0].url,
-        rating: ratingFraction()
-      }
-
-      return stateObject;
+    this.setState({componentMounted: true});
   }
 
   actionBtnClick(buttonLocation) {
@@ -174,57 +37,72 @@ class ProductCard extends React.Component {
     }
   }
 
-  render() {
-    //console.log('PRODUCT CARD RENDER------');
+  getRatingFraction(ratings) {
+    let ratingFraction;
+    let [count, sum] = [0, 0];
 
+    for (let key in ratings) {
+      count += parseInt(ratings[key]);
+      sum += parseInt(key) * parseInt(ratings[key]);
+    }
+
+    const average = sum / count;
+    return average > 0 ? ratingFraction = average : ratingFraction = 0;
+  }
+
+  render() {
     const { productCardId, updateAppProductId, currentList, hidden, cardData } = this.props;
-    const { rating,
-      features, currentItemFeatures, displayModal } = this.state;
+    const { displayModal, componentMounted } = this.state;
 
     let compareModal;
     if (currentList === 'related') {
       compareModal = <CompareModal relatedItemName={name}
-          relatedItemFeatures={features}
+          //relatedItemFeatures={features}
           displayModal={displayModal}
-          currentItemFeatures={currentItemFeatures}
+          //currentItemFeatures={currentItemFeatures}
           actionBtnClick={this.actionBtnClick} /> ;
     }
 
-    console.log('CARD DATA: ', cardData);
-
-    let [ category, name, price, sale ] = [ '...', '...', '...', '...'];
+    let [ category, name, price, sale, rating ] = [ '...', '...', '...', '...', 0];
     let primaryImg = 'https://tinyurl.com/5nur3x7w';
 
     if (cardData) {
+      console.log('assembling card data from the cache!');
       category = cardData.details.category;
       name = cardData.details.name;
       price = cardData.styles.results[0].original_price;
       sale = cardData.styles.results[0].sale;
+      rating = this.getRatingFraction(cardData.reviews.ratings);
 
       const photoPath = cardData.styles.results[0].photos[0].url;
       if (photoPath) { primaryImg = photoPath };
     }
 
-    return (
-      <div>
-        {compareModal}
-        <div className={hidden ? 'product-card hidden' : 'product-card'}>
-          <ActionButton actionBtnClick={this.actionBtnClick}
-            currentList={currentList}/>
-            <img className='card-img' src={primaryImg}
-              onClick={() => { updateAppProductId(productCardId, productObjectToCache); }}>
-              </img>
-            <div className='card-info'
-              onClick={() => { updateAppProductId(productCardId, productObjectToCache); }}>
-              <p className='card-category'>{category}</p>
-              <p className='card-name'>{name}</p>
-              <p className='card-price'>{'$' + price}</p>
-              <p className='card-sale'>{sale}</p>
-            </div>
-            <StarRating className='card-rating' rating={rating}/>
+    if (componentMounted) {
+      return (
+        <div>
+          {/* {compareModal}
+           */}
+          <div className={hidden ? 'product-card hidden' : 'product-card'}>
+            <ActionButton actionBtnClick={this.actionBtnClick}
+              currentList={currentList}/>
+              <img className='card-img' src={primaryImg}
+                onClick={() => { updateAppProductId(productCardId); }}>
+                </img>
+              <div className='card-info'
+                onClick={() => { updateAppProductId(productCardId); }}>
+                <p className='card-category'>{category}</p>
+                <p className='card-name'>{name}</p>
+                <p className='card-price'>{'$' + price}</p>
+                <p className='card-sale'>{sale}</p>
+              </div>
+              <StarRating className='card-rating' rating={rating}/>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return <div></div>;
+    }
   }
 }
 
